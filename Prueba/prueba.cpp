ed633ac004b306ae229c0715173e92766238f383
prg-77 <pabloramgav@gmail.com>
2023-03-11 19:46:02 +0100

    Añado prueba.cpp

---
+++ Prueba/prueba.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cout << "Hello" << endl; 
+    cout << "No me lo puedo creer" << endl;
+    for(i=1; i<=50; i++)
+    {
+        cout << i << endl;
+    }  
+}
