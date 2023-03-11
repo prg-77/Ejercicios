8d404b237c292d9c411ff62e5551e01517829821
prg-77 <pabloramgav@gmail.com>
2023-03-11 23:30:12 +0100

    actualizacion

--- Prueba/prueba.cpp
+++ Prueba/prueba.cpp
@@ -1,16 +1,33 @@
-#include <iostream>
+#include <iostream>  // contiene cin y cout
 #include <vector>
-#include <string>
+#include <string> // para trabajar con cadenas de caracteres
+#include <cmath> // contiene funciones matematicas
+#include <fstream> //contiene funciones para tratamiento de ficheros
+
+#define PI 3.1415
 
 using namespace std;
 
+int suma(int x, int y);
+
 int main()
 {
-    int i;
+    int i, x, y;
     cout << "Hello" << endl; 
-    cout << "No me lo puedo creer" << endl;
+    cout << "Introduce x: ";
+    cin >> x;
+    cout << "Introduce y: ";
+    cin >> y;
+    cout << "La suma es: " << suma(x,y) << endl;
     for(i=1; i<=50; i++)
     {
         cout << i << endl;
     }  
+
+    return 0;
+}
+
+int suma (int x, int y)
+{
+    return x+y;
 }
